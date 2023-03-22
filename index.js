f3f05135762599202b7ff3d142487485d7b986b1
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-22 15:30:44 +0300

    Complete Assignmentr

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
 
 /* 
@@ -20,6 +27,19 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+	console.log("Expecting: ih");
+	console.log("=>", reverseString("hi"));
+
+	console.log("");
+
+	console.log("Expecting: tobor");
+	console.log("=>", reverseString("robot"));
+
+	console.log("");
+
+	console.log("Expecting: mom");
+	console.log("=>", reverseString("mom"));
 }
 
 module.exports = isPalindrome;
